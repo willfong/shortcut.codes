cb32e8d334ed672aa497974924a553f44df759c9
Will Fong <willfong@hey.com>
2023-01-04 21:22:11 +0800

    Support ALB Health Check

--- src/index.js
+++ src/index.js
@@ -1,40 +1,44 @@
-const express = require('express');
-const morgan = require('morgan');
-const axios = require('axios');
+const express = require("express");
+const morgan = require("morgan");
+const axios = require("axios");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
-    res.redirect("https://github.com/willfong/shortcut.codes");
+	res.redirect("https://github.com/willfong/shortcut.codes");
+});
+
+app.get("/alb-health-check", (req, res) => {
+	res.send("ok");
 });
 
 app.get("/:username/:file", async (req, res, next) => {
-    const {username, file} = req.params;
-    const url = `https://raw.githubusercontent.com/${username}/shortcut.codes/master/${file}`;
-    try {
-        const response = await axios.get(url);
-        res.send(response.data);
-    } catch(err) {
-        if (err.response?.status === 404) {
-            res.status(404).send(`404: NOT FOUND - ${url}`);
-        } else {
-            next(err)
-        }
-    }
+	const { username, file } = req.params;
+	const url = `https://raw.githubusercontent.com/${username}/shortcut.codes/master/${file}`;
+	try {
+		const response = await axios.get(url);
+		res.send(response.data);
+	} catch (err) {
+		if (err.response?.status === 404) {
+			res.status(404).send(`404: NOT FOUND - ${url}`);
+		} else {
+			next(err);
+		}
+	}
 });
 
-app.use(function(req, res) {
-    res.status(404).send("404: Page Not Found");
+app.use(function (req, res) {
+	res.status(404).send("404: Page Not Found");
 });
 
-app.use(function(error, req, res, next) {
-    console.log(error);
-    res.status(500).send("500: Internal Server Error");
+app.use(function (error, req, res, next) {
+	console.log(error);
+	res.status(500).send("500: Internal Server Error");
 });
 
 app.listen(port, () => {
-    console.log(`Service started on port: ${port}`);
+	console.log(`Service started on port: ${port}`);
 });
